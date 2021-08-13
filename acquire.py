c473f82d0986df0243cd901df8da37c5011d6042
Linh Quach <Linh.M.Quach1@gmail.com>
2021-08-12 22:18:03 -0500

    adding acquire file

---
+++ acquire.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import os
+
+# turn off pink warning boxes
+import warnings
+warnings.filterwarnings("ignore")
+
+os.path.isfile('survey.csv')
+
+
+
+
+def get_survey_data(csv_file, cached=True):
+    '''
+    This function reads in external csv file for survey data and writes data to
+    a csv file if cached == False or if cached == True reads in survey data from a csv file, returns df.
+    '''
+    df = pd.read_csv(csv_file)
+    
+    if cached == False or os.path.isfile('survey.csv') == False:
+        
+        # Read fresh data from url into a DataFrame.
+        df = df
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('survey.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('survey.csv', index_col=0)
+        
+    return df
